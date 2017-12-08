90e211627593a0a3689f9380869b8de8924fad5f
ForAlMighty <weihewang2012@gmail.com>
2017-12-08 12:27:39 -0800

    Create WordBreak.py

---
+++ Dynamic_programming/WordBreak.py
@@ -0,0 +1,17 @@
+# 一定要想清楚边界，dp[i] means breakable of substring of [0,i). So for subproblem dp[j] the range is also to [0,i) 
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        wordDict = set(wordDict)
+        dp = [False] * (len(s)+1)
+        dp[0] = True
+        for i in range(1, len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+        return dp[len(s)]
