d052ac4bc2e41c397fd9d450b9f359adaadcb016
ForAlMighty <weihewang2012@gmail.com>
2017-12-16 17:21:24 -0800

    Update LetterCombo.py
    Update to deal with new test case (""). Actually for Python solution, the only parameter needed for DFS is the path. All other can be derived from the outer function.

--- LetterCombo.py
+++ LetterCombo.py
@@ -1,25 +1,24 @@
-def letterCombinations(digits):
+class Solution:
+    def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
-        def DFS(res, path, myDict, start, depth):
+        def DFS(res, path, myDict, depth):
             if depth==len(digits):
-                res.append(path)
-                return
-            for i in range(start, len(myDict[digits[depth]])):
-                DFS(res, path + myDict[digits[depth]][i], myDict, 0, depth+1)
-
-            return
+                res.append(''.join(path))
+            else:
+                for i in range(len(myDict[digits[depth]])):
+                    DFS(res, path + [myDict[digits[depth]][i]], myDict, depth+1)
 
         myDict = {'0':[' '], '1':['*'], '2': list('abc'), '3': list('def'), '4': ('ghi'),
         '5': list('jkl'), '6': list('mno'), '7': list('pqrs'), '8': list('tuv'), '9': list('wxyz')}
 
         res = []
-        path = ''
+        path = []
 
-        DFS(res, path, myDict, 0, 0)
-        return res
+        DFS(res, path, myDict, 0)
+        return res if len(digits) > 0 else []
 
 digits = "2312"
 print( letterCombinations(digits))
