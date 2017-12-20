6314e250e11c41a6114b77a4669dadfc331ebe9a
ForAlMighty <weihewang2012@gmail.com>
2017-12-19 23:10:11 -0800

    Update WordLadder_127.py
    Discard priority queue, which is over-kill.

--- BFS/WordLadder_127.py
+++ BFS/WordLadder_127.py
@@ -1,29 +1,33 @@
-from queue import PriorityQueue
-class Solution:
-    def ladderLength(self, beginWord, endWord, wordList):
-        """
-        :type beginWord: str
-        :type endWord: str
-        :type wordList: List[str]
-        :rtype: int
-        """
-        visiting = {word: False for word in wordList}
-        visited = set()
-        words = set(wordList)
-        lowercaseletters = set(string.ascii_lowercase)
+# This is basic undirected graph BFS. Should use regular deque
+import string
+from collections import deque
 
-        q = PriorityQueue();
-        q.put((1, beginWord))
+def ladderLength(beginWord, endWord, wordList):
+    """
+    :type beginWord: str
+    :type endWord: str
+    :type wordList: List[str]
+    :rtype: int
+    """
+    words = set(wordList)
+    lowercaseletters = set(string.ascii_lowercase)
 
-        while not q.empty():
-            d, word = q.get()
-            visited.add(word)
-            if word == endWord:
-                return d
-            for i in range(len(word)):
-                for lcl in lowercaseletters:
-                    newWord = word[:i] + lcl + word[i+1:]
-                    if newWord in words and newWord not in visited and not visiting[newWord]:
-                        q.put((d + 1, newWord))
-                        visiting[newWord] = True
-        return 0
+    queue = deque();
+    queue.append((1, beginWord))
+
+    while queue:
+        d, word = queue.popleft()
+        if word == endWord:
+            return d
+        for i in range(len(word)):
+            for lcl in lowercaseletters:
+                newWord = word[:i] + lcl + word[i+1:]
+                if newWord in words:
+                    words.remove(newWord)
+                    queue.append((d + 1, newWord))
+    return 0
+
+beginWord = 'hit'
+endWord = 'cog'
+wordList = ["hot","dot","dog","lot","log","cog", "dom"]
+print (ladderLength(beginWord, endWord, wordList))
