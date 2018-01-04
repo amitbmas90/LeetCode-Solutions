abb743e4b6a9504d156a7b0bd970bcf5aefad814
ForAlMighty <weihewang2012@gmail.com>
2018-01-03 21:22:56 -0800

    Update WordBreak.py
    Added Top-down approach.

--- Dynamic_programming/WordBreak.py
+++ Dynamic_programming/WordBreak.py
@@ -1,5 +1,5 @@
 # 一定要想清楚边界，dp[i] means breakable of substring of [0,i). So for subproblem dp[j] the range is also to [0,i) 
-
+# Bottom-up approach
 class Solution(object):
     def wordBreak(self, s, wordDict):
         """
@@ -15,3 +15,25 @@ class Solution(object):
                 if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
         return dp[len(s)]
+
+# Top-down approach
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        wordDict = set(wordDict)
+        memo = {len(s): True}
+        def breakable(s, start, memo):
+            if start not in memo:
+                res = False
+                for j in range(start+1, len(s)+1):
+                    if s[start:j] in wordDict and breakable(s, j, memo):
+                        res = True
+                memo[start] = res
+            return memo[start]
+        return breakable(s, 0, memo)   
+                
+        
