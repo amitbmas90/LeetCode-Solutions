dafe4f2901195158a86d3f4f43ae68f45ad1e6fc
ForAlMighty <weihewang2012@gmail.com>
2018-01-19 16:13:25 -0800

    Added top-down approach

--- Dynamic_programming/DecodeWays_91.py
+++ Dynamic_programming/DecodeWays_91.py
@@ -17,3 +17,28 @@ class Solution:
             if 10 <= int(s[i-2:i]) <= 26:
                 memo[i] += memo[i-2]
         return memo[len(s)]
+
+    
+    # Top-down approach
+    class Solution(object):
+        def numDecodings(self, s):
+            """
+            :type s: str
+            :rtype: int
+            """
+            if len(s) == 0: return 0
+            memo = {len(s): 1}
+            def dp(i):
+                if i not in memo:
+                    res = 0
+                    if s[i] == '0': res = 0
+                    elif i == len(s) - 1: res = 1
+                    else:
+                        res += dp(i+1)
+                        if 10 <= int(s[i:i+2]) < 27:
+                            res += dp(i+2)
+                    memo[i] = res
+                return memo[i]
+
+            dp(0)
+            return memo[0]
