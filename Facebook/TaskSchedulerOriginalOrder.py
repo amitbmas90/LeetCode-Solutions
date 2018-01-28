18ea82376b35ec2cdf51ed9f69e6ef9371748539
ForAlMighty <weihewang2012@gmail.com>
2018-01-28 14:35:55 -0800

    Update TaskSchedulerOriginalOrder.py
    Shorter and cleaner solution. Initial time = 0 deal with empty task list case.

--- Facebook/TaskSchedulerOriginalOrder.py
+++ Facebook/TaskSchedulerOriginalOrder.py
@@ -1,19 +1,16 @@
 # Different from Leetcode 621, this requires tasks to be done in original order.
 # In the original problem, order doesn't matter.
-def taskScheduler(tasks, idle):
-    N = len(tasks)
-    total_time = 0
-    d = {}          # store latest time execute the same task
-    idx = 0         # task waiting to be executed
-    while idx < N:
-        total_time += 1
-        if tasks[idx] not in d:
-            d[tasks[idx]] = total_time
-            idx += 1
-        elif total_time - d[tasks[idx]] > idle:
-            d[tasks[idx]] = total_time
-            idx += 1
-    return total_time
+def taskScheduler(tasks, n):
+    d = {}
+    time = 0
+    for task in tasks:
+        time += 1
+        if task not in d or time - d[task] > n:
+            d[task] = time
+        else:
+            time = d[task] + n + 1
+            d[task] = time
+    return time
 
 tasks = ['A','A','B','C','B','B','D']
 idle = 2
