d81b3a604906a0adb1bcde32a1289a94a316e5bc
ForAlMighty <weihewang2012@gmail.com>
2018-02-16 21:06:15 -0800

    Update LetterCombo_17.py
    For better and cleaner syntax.

--- DFS/LetterCombo_17.py
+++ DFS/LetterCombo_17.py
@@ -4,21 +4,16 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
-        def DFS(res, path, myDict, depth):
-            if depth==len(digits):
-                res.append(''.join(path))
-            else:
-                for i in range(len(myDict[digits[depth]])):
-                    DFS(res, path + [myDict[digits[depth]][i]], myDict, depth+1)
-
-        myDict = {'0':[' '], '1':['*'], '2': list('abc'), '3': list('def'), '4': ('ghi'),
-        '5': list('jkl'), '6': list('mno'), '7': list('pqrs'), '8': list('tuv'), '9': list('wxyz')}
-
+        if digits == '': return []
+        d = {'0':[],'1':[], '2':list('abc'), '3': list('def'), '4': list('ghi'), '5': list('jkl'), 
+            '6': list('mno'), '7': list('pqrs'), '8': list('tuv'), '9': list('wxyz')}
+        def dfs(path, idx):
+            if idx == len(digits):
+                res.append(''.join(list(path)))
+            elif digits[idx] not in {'0', '1'}:
+                for k in d[digits[idx]]:
+                    dfs(path+[k], idx+1)
         res = []
-        path = []
-
-        DFS(res, path, myDict, 0)
-        return res if len(digits) > 0 else []
-
-digits = "2312"
-print( letterCombinations(digits))
+        dfs([], 0)
+        return res
+            
