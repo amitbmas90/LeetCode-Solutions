ff5432d2c6dc0df8e365b4e73daa9f2cb8ff693c
ForAlMighty <weihewang2012@gmail.com>
2018-02-18 11:37:07 -0800

    Update FindLeavesBinaryTree_366.py

--- BinaryTree/FindLeavesBinaryTree_366.py
+++ BinaryTree/FindLeavesBinaryTree_366.py
@@ -27,3 +27,23 @@ class Solution:
             res.append(cur)
         res.append([root.val])
         return res
+
+    
+# Resursion inspired by Java solution by @sky-xu on Leetcode discussion
+class Solution:
+    def findLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        def dfs(node):
+            if node == None: return -1
+            height = 1 + max(dfs(node.left), dfs(node.right))
+            if height + 1 > len(res): res.append(list())
+            res[height].append(node.val)
+            node.left = None
+            node.right = None
+            return height
+        dfs(root)
+        return res
